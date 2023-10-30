dd2a287d18568b0caf22c0e85d1ad6b8f159b8c7
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-10-30 23:23:53 +0600

    Sql problem

--- conceptual session/0_main.sql
+++ conceptual session/0_main.sql
@@ -1,37 +1,43 @@
---your code goes here-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - --175. Combine Two Tables
+--your code goes here-- -- -- - --175. Combine Two Tables
 
     SELECT firstname,
     lastname, city, state FROM PERSON P LEFT JOIN ADDRESS AD ON P.personId = AD.personId
 
-                                                                             -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - --181. Employees Earning More Than Their Managers
+ --181. Employees Earning More Than Their Managers
 
-                                                                             -- way 1 SELECT EMP.name AS Employee FROM Employee EMP,
-                                                                Employee MGR
-                                                                    WHERE EMP.managerId = MGR.id AND EMP.salary > MGR.salary
+ -- way 1 
+ SELECT EMP.name AS Employee FROM Employee EMP,
+  Employee MGR
+  WHERE EMP.managerId = MGR.id AND EMP.salary > MGR.salary
 
                                                                                                                   -- way 2 SELECT E.name AS Employee
                                                                                                                       FROM EMPLOYEE E
                                                                                                                           INNER JOIN EMPLOYEE M ON E.managerId = M.id
                                                                                                                                                                      WHERE E.SALARY > M.SALARY;
 
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - --Duplicate Emails
-
-                                                                                                                                   -- Way 1 : Normal way
-                                                                                                                                                  SELECT Email-- COUNT(Email)
-                                                                                                                                                      FROM PERSON
-                                                                                                                                              GROUP BY Email
-                                                                                                                                              HAVING COUNT(Email) > 1
-
-                                                                                                                                                                    --Way 2 : INNER Join
-                                                                                                                                                                              SELECT DISTINCT p1.email AS Email
-                                                                                                                                                                              FROM PERSON p1
-                                                                                                                                                                              INNER JOIN PERSON p2
-                                                                                                                                                                              ON p1.email = p2.email
-                                                                                                                                                                                            WHERE p1.id<> p2.id;
-
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - --183. Customers Who Never Order
+ -- - --Duplicate
+  -- Way 1 : Normal way
+  SELECT Email-- COUNT(Email)
+  FROM PERSON
+    GROUP BY Email
+       HAVING COUNT(Email) > 1 
+        -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- - --183. Customers Who Never Order
                                                                                                                                          SELECT name AS Customers
                                                                                                                                              FROM CUSTOMERS
                                                                                                                                                  LEFT JOIN ORDERS
-                                                                                                                                                     ON CUSTOMERS.ID = ORDERS.CUSTOMERID
+                                                                                                                                             ON CUSTOMERS.ID = ORDERS.CUSTOMERID
                                                                                                                                                                            WHERE ORDERS.CUSTOMERID IS NULL;
+
+
+-- 196. Delete Duplicate emails
+DELETE p2
+FROM Person p1 JOIN person p2
+ON p1.email = p2.email
+AND p1.Id < p2.Id
+
+-- 197. Rising Temperature
+SELECT w2.id 
+FROM Weather w1 
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate,w2.recordDate)=-1
+AND w2.temperature > w1.temperature
